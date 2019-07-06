9b95622fc36fffa43b17d36cefcfdc4bb2a13811
Ciaran Farley <ciaran@cturtle98.com>
2019-07-05 20:07:04 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,14 @@
 # https://github.com/cTurtle98/text-a-cam
 #
 
+echo "this script automates raspi-config and sets up a pi for running this software and this software only"
+
+echo "press any key to continue"
+read
+
+sudo raspi-config nonint do_expand_rootfs
+
+#more raspi-config automation here
+
 sudo apt install python3 python3-pip
 
