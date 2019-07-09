a8aae77bd64dc7d2cd1a902eb8feb07cac3e76af
cTurtle98 <ciaran@cturtle98.com>
2019-07-08 17:24:28 -0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -9,20 +9,28 @@ echo "this script automates raspi-config and sets up a pi for running this softw
 echo "press any key to continue"
 read
 
+echo "~~~~~~~~ expanding root filesystem ~~~~~~~~"
 sudo raspi-config nonint do_expand_rootfs
 
+echo "~~~~~~~~ changing locale and keyboard to US ~~~~~~~~"
 locale=en_US.UTF-8
 layout=us
 sudo raspi-config nonint do_change_locale $locale
 sudo raspi-config nonint do_configure_keyboard $layout
 
+echo "~~~~~~~~ enabling camera ~~~~~~~~"
 sudo raspi-config nonint do_camera 1
 
+echo "~~~~~~~~ enabling ssh ~~~~~~~~"
 sudo raspi-config nonint do_ssh 1
 
+echo "~~~~~~~~ enabling serial console ~~~~~~~~"
 sudo raspi-config nonint do_serial 1
 
 #more raspi-config automation here
 
-sudo apt install python3 python3-pip
+echo "~~~~~~~~ installing python ~~~~~~~~"
+sudo apt install python3 python3-pip -y
 
+echo "~~~~~~~~ thank you for running the setup script ~~~~~~~~"
+echo "~~~~~~~~ your pi should be setup now ~~~~~~~~"
