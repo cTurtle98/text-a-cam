6cb5e87e8e01dfbc0ea3aa57578817bc3d73166f
cTurtle98 <ciaran@cturtle98.com>
2019-07-10 21:48:03 -0700

    blarg

--- text-a-cam.py
+++ text-a-cam.py
@@ -6,4 +6,8 @@ this is the main orchestration program
 
 from reply_with_image import reply_with_image
 
-reply_with_image('ciaran@cturtle98.com')
+print("what email would you like to send an email to?")
+
+email = input()
+
+reply_with_image(email)
